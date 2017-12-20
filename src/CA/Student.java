bfcc89ec0f0675e2b0cd647db5e6cd230ce78ea0
ronantimmons <ronantimmons1988@gmail.com>
2017-12-20 19:46:12 +0000

    Adding CA files

---
+++ src/CA/Student.java
@@ -0,0 +1,20 @@
+package CA;
+
+/**
+ *  Author: Ronan Timmons
+ * Created: 20/12/17
+ * LUpdate: 20/12/17
+ */
+public class Student {
+
+    public enum AttendanceGrade {ABSENT, VERY_POOR, POOR, AVERAGE, GOOD, VERY_GOOD};
+
+    public AttendanceGrade getAttendanceGrade(int attendance) {
+        if (attendance < 0 || attendance > 100) throw new IllegalArgumentException();
+        if (attendance < 30) return AttendanceGrade.VERY_POOR;
+        if (attendance < 50) return AttendanceGrade.POOR;
+        if (attendance < 70) return AttendanceGrade.AVERAGE;
+        if (attendance <= 90) return AttendanceGrade.GOOD;
+        return AttendanceGrade.VERY_GOOD;
+    }
+}
